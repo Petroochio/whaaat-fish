6bbae3ac8bbe1e94d464674343ef29d6d3d380bf
Krithik Ranjan <krithik.ranjan@gmail.com>
2023-10-27 13:13:59 -0600

    fix error when player dead

--- Scenes/Actors/player.gd
+++ Scenes/Actors/player.gd
@@ -8,33 +8,34 @@ var screen_size # Size of the game window.
 var target := Vector2.ZERO
 
 func _ready():
-    screen_size = get_viewport_rect().size
+	screen_size = get_viewport_rect().size
 
 
 func _process(delta):
-    if (position.distance_to(target) > 150):
+	if (position.distance_to(target) > 150):
 #        print("big_move!")
-        big_move.emit(target)
+		big_move.emit(target)
 
-    position = position.lerp(target, 0.9 * speed * delta)
-    pass
+	position = position.lerp(target, 0.9 * speed * delta)
+	pass
 
 func set_target(t: Vector2):
-    target = t
-    
+	target = t
+	
 
 func _on_body_entered(body):
-    # 
-    # hit.emit()
+	# 
+	# hit.emit()
 
-    if(body.is_in_group("food")):
-        get_score_value.emit(1)
-    else:
-        hide()
-        queue_free()
-    body.call_me()
+	if(body.is_in_group("food")):
+		get_score_value.emit(1)
+	else:
+		hide()
+		$CollisionShape2D.set_deferred("disabled", true)
+#        queue_free()
+	body.call_me()
 
-    # print_debug(body.name == "Mob")
-    # Must be deferred as we can't change physics properties on a physics callback.
-    # $CollisionShape2D.set_deferred("disabled", true)
-    pass # Replace with function body.
+	# print_debug(body.name == "Mob")
+	# Must be deferred as we can't change physics properties on a physics callback.
+	# $CollisionShape2D.set_deferred("disabled", true)
+	pass # Replace with function body.
